585c1d6a8ff624183df9ee5266848a4afa24d3a8
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-03-05 20:56:58 +0100

    Finally,improving API si maybe a good move. Not sure. Really.
    This reverts commit 91d868bd6a10ca1f2cebf0f1d24ad5f23af5cb8d.

--- classes/splitter.php
+++ classes/splitter.php
@@ -4,7 +4,8 @@ namespace estvoyage\data;
 
 interface splitter
 {
+	function dataConsumerIs(consumer $consumer);
+	function dataUseDelimiter(data $data, data\delimiter $dataDelimiter);
 	function noDelimiterInData(data $data);
 	function delimitedDataIs(data $data);
-	function dataForDataConsumerUseDataDelimiter(data $data, consumer $dataConsumer, data\delimiter $dataDelimiter);
 }
