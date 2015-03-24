112eb922c6039a75238e90244214678b72eb2ef8
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-03-24 20:37:35 +0100

    Add consumer::dataConsumerControllerIs().

--- classes/consumer.php
+++ classes/consumer.php
@@ -5,6 +5,7 @@ namespace estvoyage\data;
 interface consumer
 {
 	function dataProviderIs(provider $provider);
+	function dataConsumerControllerIs(consumer\controller $controller);
 	function newData(data $data);
 	function noMoreData();
 }
